d8b50f861a39b0f5df225905c21d337e4f96a371
hollowman08 <cmrockandel@gmail.com>
2016-08-03 20:42:07 -0700

    Original commit

---
+++ Data Wrangling Exercise 1 - Basic Data Manipulation.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(stringdist)
+
+## Loading CSV file
+refine <- read.csv("refine_original.csv")
+
+## Standardizing company names using string distance
+codes <- c("phillips", "akzo", "van houten", "unilever")
+company <- refine$company
+strdist <- adist(company, codes)
+colnames(strdist) <- codes
+rownames(strdist) <- company
+a <- amatch(company, codes, maxDist = 4)
+refine_df <- data.frame(rawtext = company, company_name = codes[a])
+
+## Add standardized columns to original data
+refine1 <- cbind(refine, refine_df)
+refine_tbl <- tbl_df(refine1)
+
+## Split proudct code and product number into two columns
+refine_tbl1 <- refine_tbl %>% 
+     separate(Product.code...number, c("product code", "product number"))
+
+## Create lookup values for product code
+lookup_tbl <- c(p = "Smartphone", v = "TV", x = "Laptop", q = "Tablet")
+refine_tbl2 <- refine_tbl1 %>%
+     mutate(product_category = lookup_tbl[`product code`])
+
+## Group address into one column: full_address
+refine_tbl3 <- unite(refine_tbl2, full_address, address, city, country, sep = ",")
+
+## Create dummy variables for company and product categories
+refine_tbl4 <- refine_tbl3 %>% 
+     mutate(companies = paste("company", company_name, sep = "_"), 
+            product_categories = paste("product", product_category, sep = "_")) %>%
+     spread(companies, companies) %>% 
+     spread(product_categories, product_categories)
+
+refine_tbl4[,9:16] <- ifelse(!is.na(refine_tbl4[,9:16]),1,0)
+
+refine_final <- refine_tbl4 %>% 
+     select(company_name, `product code`, `product number`, full_address, name
+            ,product_category, 9:16)
+
+## write to CSV file
+write.csv(refine_final, "refine_clean.csv")
